f5f2272fe36cc7232835d86fccf8f48ff976320a
Andrea Erazo <Andrea Erazo@LAPTOP-ENO6ER5T>
2021-11-20 11:18:57 -0600

    Tarea

---
+++ src/main/java/com/mycompany/mavenproject1/NewMain.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Andrea Erazo
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+        
+    }
+    
+}
